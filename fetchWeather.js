f6aeb0d49fe31fb47011ef4b85c354f39b215a1d
Scott Geleas <scottgeleas97@gmail.com>
2022-03-02 17:21:03 -0500

    returning weather data

--- fetchWeather.js
+++ fetchWeather.js
@@ -1,24 +1,33 @@
 const apiKey = '6aab536680d25358aa622219c8452f38';
-// const weatherUrl = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`
+// getCoords takes in the geocode URL from OpenWeatherAPI and returns the latitude and longitude of the given city and state
 async function getCoords(url) {
     const response = await fetch(url);
-    const data = await response.json();
-    const location = data[0];
-    const lat = location.lat;
-    const lon = location.lon;
-    // console.log(lat, lon);
-    return {
+    const location = await response.json();
+    const lat = location[0].lat;
+    const lon = location[0].lon;
+    const coords = {
         lat,
         lon,
     };
+    return coords;
+}
+// getWeather takes in the current weather URL from OpenWeatherAPI and returns an object with weather data
+async function getWeather(url) {
+    const response = await fetch(url);
+    const weather = await response.json();
+    return weather;
 }
 
-async function returnWeather() {
-    const cityName = 'los angeles';
-    const stateCode = 'US-CA';
+async function renderWeather() {
+    const cityName = 'boston';
+    const stateCode = 'US-MA';
     const geocodeUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${stateCode}&appid=${apiKey}`;
     const coords = await getCoords(geocodeUrl);
-    console.log(coords);
+    const lat = coords.lat;
+    const lon = coords.lon;
+    const weatherUrl = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+    const weather = await getWeather(weatherUrl);
+    console.log(weather, coords);
 }
 
-returnWeather();
+renderWeather();
