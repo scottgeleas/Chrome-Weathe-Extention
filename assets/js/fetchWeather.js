6e2005cfe0140bc011292ada17355966d5233f9e
Scott Geleas <scottgeleas97@gmail.com>
2022-03-02 18:17:07 -0500

    basic styles

--- assets/js/fetchWeather.js
+++ assets/js/fetchWeather.js
@@ -28,6 +28,7 @@ async function renderWeather() {
     const weatherUrl = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
     const weather = await getWeather(weatherUrl);
     console.log(weather, coords);
+
 }
 
 renderWeather();
