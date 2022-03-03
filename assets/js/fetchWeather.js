548a8bbbb2e4cbb935ddfc5a471e18a4138afa82
Scott Geleas <scottgeleas97@gmail.com>
2022-03-02 20:18:24 -0500

    refactored state code with object and function

--- assets/js/fetchWeather.js
+++ assets/js/fetchWeather.js
@@ -2,11 +2,17 @@ const apiKey = '6aab536680d25358aa622219c8452f38';
 const form = document.querySelector('form');
 const cityInput = document.getElementById('city-input');
 const stateInput = document.getElementById('state-input');
+const iconEl = document.querySelector('img');
+const tempEl = document.getElementById('temp');
+const descriptionEl = document.getElementById('description');
+const humidityEl = document.getElementById('humidity');
+const windEl = document.getElementById('wind');
 
 // getCoords takes in the geocode URL from OpenWeatherAPI and returns the latitude and longitude of the given city and state
 async function getCoords(url) {
     const response = await fetch(url);
     const location = await response.json();
+    // console.log(location);
     const lat = location[0].lat;
     const lon = location[0].lon;
     const coords = {
@@ -22,175 +28,82 @@ async function getWeather(url) {
     return weather;
 }
 
+const stateCodes = {
+    alaska: 'US-AK',
+    alabama: 'US-AL',
+    arkansas: 'US-AR',
+    arizona: 'US-AZ',
+    california: 'US-CA',
+    colorado: 'US-CO',
+    connecticut: 'US-CT',
+    delaware: 'US-DE',
+    florida: 'US-FL',
+    georgia: 'US-GA',
+    hawaii: 'US-HI',
+    iowa: 'US-IA',
+    idaho: 'US-ID',
+    illinois: 'US-IL',
+    indiana: 'US-IN',
+    kansas: 'US-KS',
+    kentucky: 'US-KY',
+    louisiana: 'US-LA',
+    massachusetts: 'US-MA',
+    maryland: 'US-MD',
+    maine: 'US-ME',
+    michigan: 'US-MI',
+    minnesota: 'US-MN',
+    missouri: 'US-MO',
+    mississippi: 'US-MS',
+    montana: 'US-MT',
+    northcarolina: 'US-NC',
+    northdakota: 'US-ND',
+    nebraska: 'US-NE',
+    newhampshire: 'US-NH',
+    newjersey: 'US-NJ',
+    newmexico: 'US-NM',
+    nevada: 'US-NV',
+    newyork: 'US-NY',
+    ohio: 'US-OH',
+    oklahoma: 'US-OK',
+    oregon: 'US-OR',
+    pennsylvania: 'US-PA',
+    rhodeisland: 'US-RI',
+    southcarolina: 'US-SC',
+    southdakota: 'US-SD',
+    tennessee: 'US-TN',
+    texas: 'US-TX',
+    utah: 'US-UT',
+    virginia: 'US-VA',
+    vermont: 'US-VT',
+    washington: 'US-WA',
+    wisconsin: 'US-WI',
+    westvirginia: 'US-WV',
+    wyoming: 'US-WY',
+};
+
+const getStateCode = (state) => {
+    return stateCodes[state];
+};
+
 async function renderWeather(e) {
     e.preventDefault();
-    
+
     const cityName = cityInput.value.toLowerCase().trim();
-    const state = stateInput.value.toLowerCase().trim();
-    console.log(cityName, state);
-    let stateCode = ''
-
-    switch (state) {
-        case 'alaska':
-            stateCode = 'US-AK';
-            break;
-        case 'alabama':
-            stateCode = 'US-AL';
-            break;
-        case 'arkansas':
-            stateCode = 'US-AR';
-            break;
-        case 'arizona':
-            stateCode = 'US-AZ';
-            break;
-        case 'california':
-            stateCode = 'US-CA';
-            break;
-        case 'colorado':
-            stateCode = 'US-CO';
-            break;
-        case 'connecticut':
-            stateCode = 'US-CT';
-            break;
-        case 'delaware':
-            stateCode = 'US-DE';
-            break;
-        case 'florida':
-            stateCode = 'US-FL';
-            break;
-        case 'georgia':
-            stateCode = 'US-GA';
-            break;
-        case 'hawaii':
-            stateCode = 'US-HI';
-            break;
-        case 'iowa':
-            stateCode = 'US-IA';
-            break;
-        case 'idaho':
-            stateCode = 'US-ID';
-            break;
-        case 'illinois':
-            stateCode = 'US-IL';
-            break;
-        case 'indiana':
-            stateCode = 'US-IN';
-            break;
-        case 'kansas':
-            stateCode = 'US-KS';
-            break;
-        case 'kentucky':
-            stateCode = 'US-KY';
-            break;
-        case 'louisiana':
-            stateCode = 'US-LA';
-            break;
-        case 'massachusetts':
-            stateCode = 'US-MA';
-            break;
-        case 'maryland':
-            stateCode = 'US-MD';
-            break;
-        case 'maine':
-            stateCode = 'US-ME';
-            break;
-        case 'michigan':
-            stateCode = 'US-MI';
-            break;
-        case 'minnesota':
-            stateCode = 'US-MN';
-            break;
-        case 'missouri':
-            stateCode = 'US-MO';
-            break;
-        case 'mississippi':
-            stateCode = 'US-MS';
-            break;
-        case 'montana':
-            stateCode = 'US-MT';
-            break;
-        case 'north carolina':
-            stateCode = 'US-NC';
-            break;
-        case 'north dakota':
-            stateCode = 'US-ND';
-            break;
-        case 'nebraska':
-            stateCode = 'US-NE';
-            break;
-        case 'new hampshire':
-            stateCode = 'US-NH';
-            break;
-        case 'new jersey':
-            stateCode = 'US-NJ';
-            break;
-        case 'new mexico':
-            stateCode = 'US-NM';
-            break;
-        case 'nevada':
-            stateCode = 'US-NV';
-            break;
-        case 'new york':
-            stateCode = 'US-NY';
-            break;
-        case 'ohio':
-            stateCode = 'US-OH';
-            break;
-        case 'oklahoma':
-            stateCode = 'US-OK';
-            break;
-        case 'oregon':
-            stateCode = 'US-OR';
-            break;
-        case 'pennsylvania':
-            stateCode = 'US-PA';
-            break;
-        case 'rhode island':
-            stateCode = 'US-RI';
-            break;
-        case 'south carolina':
-            stateCode = 'US-SC';
-            break;
-        case 'south dakota':
-            stateCode = 'US-SD';
-            break;
-        case 'tennessee':
-            stateCode = 'US-TN';
-            break;
-        case 'texas':
-            stateCode = 'US-TX';
-            break;
-        case 'utah':
-            stateCode = 'US-UT';
-            break;
-        case 'virginia':
-            stateCode = 'US-VA';
-            break;
-        case 'vermont':
-            stateCode = 'US-VT';
-            break;
-        case 'washington':
-            stateCode = 'US-WA';
-            break;
-        case 'wisconsin':
-            stateCode = 'US-WI';
-            break;
-        case 'west virginia':
-            stateCode = 'US-WV';
-            break;
-        case 'wyoming':
-            stateCode = 'US-WY';
-            break;
-    }
-
-
-    const geocodeUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${stateCode}&appid=${apiKey}`;
+    const state = stateInput.value.toLowerCase().trim().split(' ').join('');
+    const stateCode = getStateCode(state)
+    console.log(state, stateCode);
+    const geocodeUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${stateCode},US&appid=${apiKey}`;
     const coords = await getCoords(geocodeUrl);
     const lat = coords.lat;
     const lon = coords.lon;
-    const weatherUrl = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+    const weatherUrl = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
     const weather = await getWeather(weatherUrl);
-    console.log(weather, coords);
+    // console.log(weather);
+    descriptionEl.textContent = weather.weather[0].main;
+    tempEl.textContent = `Current Temperature: ${weather.main.temp}°F`;
+    humidityEl.textContent = `Humidity: ${weather.main.humidity}%`;
+    windEl.textContent = `Wind Speed: ${weather.wind.speed}mph`;
+    // iconEl.setAttribute('src', weather.weather[0].icon)
 }
 
 form.addEventListener('submit', renderWeather);
